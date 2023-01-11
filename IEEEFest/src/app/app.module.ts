83d3ebecc12be21dcef15b742ba6c5e319afcbeb
Harshada <harshada.jagade@gmail.com>
2023-01-11 23:10:54 +0530

    sponsor

--- IEEEFest/src/app/app.module.ts
+++ IEEEFest/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { SponsorComponent } from './sponsor/sponsor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    SponsorComponent
   ],
   imports: [
     BrowserModule,
