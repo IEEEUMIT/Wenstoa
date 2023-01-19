d8743020e07874db891f67481992eb6a1999d9b0
SUMA63 <gsuma577@gmail.com>
2023-01-19 20:21:25 +0530

    footer

--- IEEEFest/src/app/app.module.ts
+++ IEEEFest/src/app/app.module.ts
@@ -19,7 +19,7 @@ import { FooterComponent } from './footer/footer.component';
     SponsorComponent,
     EventsComponent,
     TimelineComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
