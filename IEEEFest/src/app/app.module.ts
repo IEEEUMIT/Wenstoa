256b5bed140009f9e1df2486d90faea9f4e6c445
SUMA63 <gsuma577@gmail.com>
2023-01-20 23:43:13 +0530

    events

--- IEEEFest/src/app/app.module.ts
+++ IEEEFest/src/app/app.module.ts
@@ -9,14 +9,9 @@ import { SponsorComponent } from './sponsor/sponsor.component';
 import { EventsComponent } from './MyComponent/events/events.component';
 import { TimelineComponent } from './timeline/timeline.component';
 import { FooterComponent } from './footer/footer.component';
-<<<<<<< HEAD
-import { FaqComponent } from './faq/faq.component';
-import { FaqListComponent } from './faq-list/faq-list.component';
-=======
 import { EventpageComponent } from './eventpage/eventpage.component';
 
 
->>>>>>> 337289cddd86d79ec9adf4c84cf1c01eecdd4e1e
 
 @NgModule({
   declarations: [
@@ -27,12 +22,7 @@ import { EventpageComponent } from './eventpage/eventpage.component';
     EventsComponent,
     TimelineComponent,
     FooterComponent,
-<<<<<<< HEAD
-    FaqComponent,
-    FaqListComponent
-=======
     EventpageComponent
->>>>>>> 337289cddd86d79ec9adf4c84cf1c01eecdd4e1e
   ],
   imports: [
     BrowserModule,
