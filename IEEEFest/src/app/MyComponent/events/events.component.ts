8ae5bac75e6e1efd1ad5e152fd7564b36362687c
SUMA63 <gsuma577@gmail.com>
2023-01-20 20:32:44 +0530

    events

--- IEEEFest/src/app/MyComponent/events/events.component.ts
+++ IEEEFest/src/app/MyComponent/events/events.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { events } from 'src/app/JSONdata/events';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit{
+  // evens:any;
+  // ngOnInit(): void {
+  //   this.evens=events
+  // }
+  eventdel:any;
+  constructor() {}
+  
+  ngOnInit(): void {
+    this.eventdel=events;
+  }
+
 
 }
