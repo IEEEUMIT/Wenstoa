9e492f6035eef67991614342af7a5a0a2eff057a
rifatperween <rifatperween02@gmail.com>
2023-01-21 00:04:06 +0530

    timeline

--- IEEEFest/src/app/MyComponent/events/events.component.ts
+++ IEEEFest/src/app/MyComponent/events/events.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { events } from 'src/app/JSONdata/events';
+// import { events } from 'src/app/JSONDATA/events'; change
+import { events } from 'src/app/JSONdata/events'; 
+
 
 @Component({
   selector: 'app-events',
