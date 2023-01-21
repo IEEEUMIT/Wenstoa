426f611954808e0431ae1716fd36224401f83199
mokshadapatil <mokshadapatil0183@gmail.com>
2023-01-21 17:45:05 +0530

    footer

--- IEEEFest/src/app/MyComponent/events/events.component.ts
+++ IEEEFest/src/app/MyComponent/events/events.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { events } from 'src/app/JSONDATA/events'; 
 
-import { events } from 'src/app/JSONDATA/events'; 
-// import { events } from 'src/app/JSONdata/events'; 
+
 
 
 @Component({
