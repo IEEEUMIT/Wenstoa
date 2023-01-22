a3665a5eaafa538937aa2f375ecd3e4f23cdfa7d
ArchanaKalathiya <archanakalathiya91@gmail.com>
2023-01-22 17:04:44 +0530

    faq

--- IEEEFest/src/app/JSONDATA/faq.ts
+++ IEEEFest/src/app/JSONDATA/faq.ts
@@ -3,6 +3,29 @@ export var FAQ = [
         "question": "What is 360 Degrees + ?",
         "answer":"The 360 Degrees Plus is the technical fest of IEEE UMIT It helps students strengthen their knowledge and hone their technical skills",
         
-    }
-    
+    },
+    {
+        "question":"",
+        "answer":"",
+    },
+    {
+        "question":"",
+        "answer":"",
+    },
+    {
+        "question":"",
+        "answer":"",
+    },
+    {
+        "question":"",
+        "answer":"",
+    },
+    {
+        "question":"",
+        "answer":"",
+    },
+    {
+        "question":"",
+        "answer":"",
+    },
 ]
