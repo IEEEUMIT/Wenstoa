2eeb09f2fb1b481ca750e6e48225522e17d5d529
ArchanaKalathiya <archanakalathiya91@gmail.com>
2023-01-22 23:08:57 +0530

    Eventpage

--- IEEEFest/src/app/event/event.component.ts
+++ IEEEFest/src/app/event/event.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { event } from '../JSONDATA/event';
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.css']
 })
 export class EventComponent {
+  eventdetail:any;
+  constructor() {}
+  
+  ngOnInit(): void {
+    this.eventdetail=event;
+  }
 
 }
