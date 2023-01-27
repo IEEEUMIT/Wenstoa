1d21389805abd0b283e01572e2982ded0f531c89
ArchanaKalathiya <archanakalathiya91@gmail.com>
2023-01-27 22:58:34 +0530

    Main div

--- IEEEFest/src/app/home/home.component.ts
+++ IEEEFest/src/app/home/home.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { event } from '../JSONDATA/event';
 import { footer } from '../JSONDATA/footer';
 import { sponsors } from '../JSONDATA/sponsor';
-
+import { FAQ } from '../JSONDATA/faq';
+import { timeline } from '../JSONDATA/timeline';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  
+  timelinedel:any;   
+  faq:any = [];
+  faqDisplay: Array<Boolean> = []
   footer: any;
   spons:any;
   user:any;
   ngOnInit(): void{
     this.footer = footer ;
-    this.spons=sponsors
+    this.spons=sponsors ;
+    this.timelinedel=timeline;
+    this.faq=FAQ;
+    for(let i of this.faq){
+      this.faqDisplay.push(false);
+    }
+  }
+  toggleFAQ(idx: number){
+    this.faqDisplay[idx] = !this.faqDisplay[idx];
   }
 }
 
