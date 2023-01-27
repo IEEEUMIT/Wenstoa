6621f5a8b357a0b8c3b00f02ca4037087705daf5
ArchanaKalathiya <archanakalathiya91@gmail.com>
2023-01-27 15:54:56 +0530

    FAQ

--- IEEEFest/src/app/MyComponent/events/events.component.ts
+++ IEEEFest/src/app/MyComponent/events/events.component.ts
@@ -1,20 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { events } from 'src/app/JSONDATA/events'; 
 
-<<<<<<< HEAD
-
-
-
-import { event } from 'src/app/JSONDATA/event'; 
-
-
-// import {events} from 'src/app/JSONDATA/events'; 
-
-// import { events} from 'src/app/JSONDATA/events'; 
-
-
-=======
->>>>>>> eb15fc1c5c41d8652b898645eca8ff13286ae68b
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
