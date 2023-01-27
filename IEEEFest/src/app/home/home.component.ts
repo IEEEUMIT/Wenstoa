634018ab034d75ac5802e665e993860b40663acc
SUMA63 <gsuma577@gmail.com>
2023-01-27 19:51:27 +0530

    event

--- IEEEFest/src/app/home/home.component.ts
+++ IEEEFest/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { event } from '../JSONDATA/event';
 import { footer } from '../JSONDATA/footer';
 import { sponsors } from '../JSONDATA/sponsor';
+import { events } from 'src/app/JSONDATA/events';
+
 
 @Component({
   selector: 'app-home',
@@ -12,9 +14,11 @@ export class HomeComponent {
   footer: any;
   spons:any;
   user:any;
+  eventdel: any;
   ngOnInit(): void{
     this.footer = footer ;
-    this.spons=sponsors
+    this.spons=sponsors;
+    this.eventdel=events;
   }
 }
 
