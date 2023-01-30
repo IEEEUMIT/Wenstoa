eda588e22cfe45008a701a963cb6943a32f0b562
akanksha <mohiteakanksha313@gmail.com>
2023-01-29 23:18:30 -0800

    faq

--- IEEEFest/src/app/JSONDATA/faq.ts
+++ IEEEFest/src/app/JSONDATA/faq.ts
@@ -6,7 +6,7 @@ export var FAQ = [
     },
     {
         "question":"What are the 360 Degrees + dates?",
-        "answer":"The dates of 360+ are February 3–5. Prepare for the three craziest days of the year!",
+        "answer":"The dates of 360+ are February 3 and 4. Prepare for the two craziest days of the year!",
     },
     {
         "question":"What is this year's theme ?",
@@ -26,10 +26,10 @@ export var FAQ = [
     },
     {
         "question":"How many Technical and Non-technical events are organized ?",
-        "answer":"IEEE Fest organizes 4 Technical and 5 Non-technical events",
+        "answer":"IEEE Fest organizes 4 Technical and 4 Non-technical events along with a fun treasure hunt event.",
     },
     {
-        "question":"Do I get a break if I submit many applications?",
+        "question":"Do I get a discount if I participate in many events?",
         "answer":"No, there isn't such a policy; but, if you are a member of IEEE, you will receive a discount.",
     },
     {
@@ -46,7 +46,7 @@ export var FAQ = [
     },
     {
         "question":"Will I get a certificate for participation?",
-        "answer":"Yes everyone participating will get a certificate of participation.",
+        "answer":"Yes if you have participated in 2 non-technical and 1 technical event.",
     },
     {
         "question":"Can people of any college attend 360 Degrees +?",
