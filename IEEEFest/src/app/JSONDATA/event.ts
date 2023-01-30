cb2275517e9f6e5f848623861b37171f14b22389
Riddhi278 <104679929+Riddhi278@users.noreply.github.com>
2023-01-30 19:49:33 +0530

    event

--- IEEEFest/src/app/JSONDATA/event.ts
+++ IEEEFest/src/app/JSONDATA/event.ts
@@ -1,10 +1,11 @@
 export var event = 
 [{   "id": 7, 
     "name": "Ravzion",
-    "type": "Life is full of riddles, So while exploring and solving one another, don't forget to enjoy your journey too",
+    "type": "Life is full of riddles, So while exploring and solving one another,",
+    "type2":"don't forget to enjoy your journey too",
     "tagline": "Wander wisely",
-    "member":"500",
-    "nonmember":"600",
+    "member":"free",
+    "nonmember":"free",
     "date" : "2",
     "time":[
         "round 1: 11 to 12",
@@ -16,7 +17,7 @@ export var event =
         {
             "name":"Tanvee Joshi",
             "emailid":"tanvee03joshi@gmail.com",
-            "number":"5667889433",
+            "number":"9028249474",
             
         },
 
@@ -28,19 +29,20 @@ export var event =
 
         {
             "name":"Pratiksha Barman",
-            "emailid":"gdfscjdt",
+            "emailid":" pratikshabarman208@gmail.com",
             "number":"7977605183",
         },
     ],
-   "prizes": ["566","ghh"],
+   "prizes": ["will reveal soon!"],
    "rules" : ["In case of any discrepancy, the organizers will have a final say."],
 },
 {    "id":6 ,
     "name":"Mahfil-E-Taxi",
     "type": "*Happiness is*....a taxi appearing just when you need it...So board the taxi",
     "type2":"Dil ki dastaan sunnao tumhari Jarur aao mehfil me humari",
-
     "tagline": "Raah me milte lafz",
+    "member":"20",
+    "nonmember":"25",
     "date" : "56 65",
     "time":[
         "round 1: 12 to 13",
@@ -62,12 +64,12 @@ export var event =
 
         {
             "name":"Pratiksha Barman",
-            "emailid":"gdfscjdt",
+            "emailid":" pratikshabarman208@gmail.com",
             "number":"7977605183",
             
        },
     ],
-  "prizes": ["567","667"],
+  "prizes": ["Will reveal soon!!"],
   "rules" : ["hgfcbnhd hgsxfggrwwx jhgfghhy jhyreecbhj jjytvvbnjjy "],
 }, 
 
@@ -78,6 +80,8 @@ export var event =
     "type": "Give your  touch to design and make websites come alive beacause Great",
     "type2":"web design is more than pretty pictures",
     "tagline": "Awaken Your Inner Srujana ",
+    "member":"Free",
+    "nonmember":"Free",
     "date" : "56 65",
     "time":[
         "round 1: 12 to 13",
@@ -99,12 +103,12 @@ export var event =
 
         {
             "name":"Pratiksha Barman",
-            "emailid":"gdfscjdt",
+            "emailid":" pratikshabarman208@gmail.com",
             "number":"7977605183",
             
        },
     ],
-  "prizes": ["567","667"],
+  "prizes": ["Will reveal soon!!"],
   "rules" : ["hgfcbnhd hgsxfggrwwx jhgfghhy jhyreecbhj jjytvvbnjjy "]
 }, 
 {    "id" : 5,
@@ -112,6 +116,8 @@ export var event =
     "type": "Life toh hai out of control, but we are here to take full control of your enteLrtainment.",
     "type2":"A fun event, with games of a kind that will help you create memories of a lifetime!",
     "tagline": "Bazm -e-yaaraan",
+    "member":"Free",
+    "nonmember":"Free",
     "date" : "56 65",
     "time":[
         "round 1: 12 to 13",
@@ -133,12 +139,12 @@ export var event =
 
         {
             "name":"Pratiksha Barman",
-            "emailid":"gdfscjdt",
+            "emailid":"pratikshabarman208@gmail.com",
             "number":"7977605183",
             
        },
     ],
-  "prizes": ["567","667"],
+  "prizes": ["Will reveal soon!!"],
   "rules" : ["hgfcbnhd hgsxfggrwwx jhgfghhy jhyreecbhj jjytvvbnjjy "]
 }, 
 {   "id" : 1,
@@ -146,7 +152,9 @@ export var event =
     "type": "he best error message is the one that never shows up.Experience is the name everyone gives ",
     "type2":"to their mistakes So come and build the experience you want.",
     "tagline": "Think.Code.Execute",
-    "date" : "56 65",
+    "member":"500",
+    "nonmember":"Free",
+    "date" : "Free",
     "time":[
         "round 1: 12 to 13",
         "round 2: 11 to 16",
@@ -167,12 +175,12 @@ export var event =
 
         {
             "name":"Pratiksha Barman",
-            "emailid":"gdfscjdt",
+            "emailid":"pratikshabarman208@gmail.com",
             "number":"7977605183",
             
        },
     ],
-  "prizes": ["567","667"],
+  "prizes": ["Will reveal soon!"],
   "rules" : ["hgfcbnhd hgsxfggrwwx jhgfghhy jhyreecbhj jjytvvbnjjy "]
 }, 
 {   "id" : 2,
@@ -180,6 +188,8 @@ export var event =
     "type": "Best practices are those practices that generally produce the best results or minimize risk.",
      "type2": "So come and give a try!!",
     "tagline": "On The Hunt For The One",
+    "member":"40",
+    "nonmember":"45",
     "date" : "56 65",
     "time":[
         "round 1: 12 to 13",
@@ -201,12 +211,12 @@ export var event =
 
         {
             "name":"Pratiksha Barman",
-            "emailid":"gdfscjdt",
+            "emailid":" pratikshabarman208@gmail.com",
             "number":"7977605183",
             
        },
     ],
-  "prizes": ["567","667"],
+  "prizes": [" ₹500"],
   "rules" : ["hgfcbnhd hgsxfggrwwx jhgfghhy jhyreecbhj jjytvvbnjjy "]
 }, 
 
@@ -214,7 +224,9 @@ export var event =
     "name":"TechTalkies",
     "type": "An opportunity to showcase your knowledge should never be wasted.",
     "type2":"So what are you waiting for? Tech talkies gives you the platform to show off your skills",
-    "tagline": "hdfsv",
+    "tagline": "Unleashing Ideas Speak your mind",
+    "member":"40",
+    "nonmember":"45",
     "date" : "56 65",
     "time":[
         "round 1: 12 to 13",
@@ -236,12 +248,12 @@ export var event =
 
         {
             "name":"Pratiksha Barman",
-            "emailid":"gdfscjdt",
+            "emailid":" pratikshabarman208@gmail.com",
             "number":"7977605183",
             
        },
     ],
-  "prizes": ["567","667"],
+  "prizes": [" ₹500"],
   "rules" : ["hgfcbnhd hgsxfggrwwx jhgfghhy jhyreecbhj jjytvvbnjjy "]
 },
 {    "id" : 8,
@@ -249,6 +261,8 @@ export var event =
     "type": "Kuch toh gadbad hai DAYA.... Everything is a mystery until you unlock it.",
     "type2":"Be a detective for a day. Unleash the power of your mind",
     "tagline": "Ghum Naam Hai Koi,Badnaam Hai Koi",
+    "member":"20 per person",
+    "nonmember":"20 person",
     "date" : "56 65",
     "time":[
         "round 1: 12 to 13",
@@ -270,12 +284,49 @@ export var event =
 
         {
             "name":"Pratiksha Barman",
-            "emailid":"gdfscjdt",
+            "emailid":" pratikshabarman208@gmail.com",
             "number":"7977605183",
             
        },
     ],
-  "prizes": ["567","667"],
+  "prizes": ["Will reveal soon!"],
   "rules" : ["hgfcbnhd hgsxfggrwwx jhgfghhy jhyreecbhj jjytvvbnjjy "]
 }, 
+{
+    "id" : 9,
+"name":"Phir Hera Pheri ",
+"type": "Treasure Hunt",
+"tagline": "Paisa Hi Paisa Hoga",
+"member":"25 per person",
+"nonmember":"25 per person",
+"date" : "56 65",
+"time":[
+    "round 1: 12 to 13",
+    "round 2: 11 to 16",
+],
+"eventincharge":
+[
+    {
+        "name":"Tanvee Joshi",
+        "emailid":"tanvee03joshi@gmail.com",
+        "number":"9028249474",
+    },
+
+    { "name":"Kshitija Karande",
+    "emailid":"kshitijaak2502@gmail.com",
+    "number":"8788354162",
+        
+    },
+
+    {
+        "name":"Pratiksha Barman",
+        "emailid":" pratikshabarman208@gmail.com",
+        "number":"7977605183",
+        
+   },
+],
+"prizes": [" ₹400"],
+"rules" : ["hgfcbnhd hgsxfggrwwx jhgfghhy jhyreecbhj jjytvvbnjjy "]
+}, 
+
 ]
