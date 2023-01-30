8c3fbf1a910dda825ebeb56ca2995411e70d515e
akanksha <mohiteakanksha313@gmail.com>
2023-01-30 03:00:02 -0800

    FAQ

--- IEEEFest/src/app/JSONDATA/faq.ts
+++ IEEEFest/src/app/JSONDATA/faq.ts
@@ -1,12 +1,12 @@
 export var FAQ = [
     {
         "question": "What is 360 Degrees + ?",
-        "answer":"It is technical fest of IEEE UMIT.It helps students strengthen their knowledge and technical skills.",
-        
+        "answer":"It is technical fest of IEEE UMIT which helps students improve their technical skills.",
     },
     {
         "question":"What are the 360 Degrees + dates?",
-        "answer":"The dates of 360+ are February 3 and 4. Prepare for the two craziest days of the year!",
+        "answer":"The dates of 360+ are February 3 and 4.",
+        
     },
     {
         "question":"What is this year's theme ?",
@@ -22,15 +22,18 @@ export var FAQ = [
     },
     {
         "question":"Are there any prizes for the winners?",
-        "answer":"You will get Exciting prizes from our sponsors.So dont miss chance.",
+        "answer":"You will get cash prizes for technical events and exciting prizes for non-technical events.",
     },
     {
         "question":"How many Technical and Non-technical events are organized ?",
         "answer":"IEEE Fest organizes 4 Technical and 4 Non-technical events along with a fun treasure hunt event.",
+    
+        
     },
     {
         "question":"Do I get a discount if I participate in many events?",
-        "answer":"No, there isn't such a policy; but, if you are a member of IEEE, you will receive a discount.",
+        "answer":"No, there isn't such a policy;but,if you are a member of IEEE, you will receive a discount.",
+    
     },
     {
         "question":"Which competitions shall be conducted online this year?",
@@ -38,11 +41,13 @@ export var FAQ = [
     },
     {
         "question":"What is the date, time and venue for these competitions?",
-        "answer":"The detailed schedule of the competitions will be released later on our social media handle.",
+        "answer":"The detailed schedule of the competitions will be released later on our social media handle. ",
+    
     },
     {
         "question":"Am I supposed to register for the group competition myself?",
-        "answer":"If you are the team leader, you must go to the competition you are interested in and create a team and submit the required fee.",
+        "answer":"If you are the team leader, you must go to the competition you are interested in and create a team and submit the required fee. ",
+    
     },
     {
         "question":"Will I get a certificate for participation?",
